ad94c7ac386a81c4309a4d890466608b4e33d45c
Jun-0101 <junpham1101@gmail.com>
2026-01-24 15:31:41 +0100

    create Spring Boot sproject using Maven; configure dependencies; create structure follow Spring MVC

---
+++ supermarket-deals/src/main/java/com/example/supermarket_deals/SupermarketDealsApplication.java
@@ -0,0 +1,13 @@
+package com.example.supermarket_deals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketDealsApplication.class, args);
+	}
+
+}
---
+++ supermarket-deals/src/test/java/com/example/supermarket_deals/SupermarketDealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supermarket_deals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketDealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
