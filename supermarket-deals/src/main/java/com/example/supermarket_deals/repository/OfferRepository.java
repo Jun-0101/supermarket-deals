43a83b890a398d3bf42288bdd2fbca69671c3f83
Jun-0101 <junpham1101@gmail.com>
2026-01-25 11:50:44 +0100

    implement OfferRepository

---
+++ supermarket-deals/src/main/java/com/example/supermarket_deals/repository/OfferRepository.java
@@ -0,0 +1,11 @@
+package com.example.supermarket_deals.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.example.supermarket_deals.entity.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OfferRepository extends JpaRepository<Offer, Long>{
+    List<Offer> findByValidOffers(LocalDate start, LocalDate end);
+}
