be9f257d99ae8dfd19a5bf9ced8db23ae247c4fe
Jun-0101 <junpham1101@gmail.com>
2026-01-25 12:11:24 +0100

    rename function

--- supermarket-deals/src/main/java/com/example/supermarket_deals/repository/OfferRepository.java
+++ supermarket-deals/src/main/java/com/example/supermarket_deals/repository/OfferRepository.java
@@ -7,5 +7,5 @@ import com.example.supermarket_deals.entity.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfferRepository extends JpaRepository<Offer, Long>{
-    List<Offer> findByValidOffers(LocalDate start, LocalDate end);
+    List<Offer> findByValidFromLessThanEqualAndValidToGreaterThanEqual(LocalDate start, LocalDate end);
 }
