d167e3981bc44a594956c3314d7edf157fc79c47
Jun-0101 <junpham1101@gmail.com>
2026-01-25 12:34:30 +0100

    Add supermarket filter for offer

--- supermarket-deals/src/main/java/com/example/supermarket_deals/repository/OfferRepository.java
+++ supermarket-deals/src/main/java/com/example/supermarket_deals/repository/OfferRepository.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import com.example.supermarket_deals.entity.Offer;
+import com.example.supermarket_deals.entity.Supermarket;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OfferRepository extends JpaRepository<Offer, Long>{
-    List<Offer> findByValidFromLessThanEqualAndValidToGreaterThanEqual(LocalDate start, LocalDate end);
+    List<Offer> findByValidFromLessThanEqualAndValidToGreaterThanEqualAndSupermarket(
+        LocalDate start, LocalDate end, Supermarket supermarket);
 }
